5c94afe2a258ba13a159269a128776b5d65c80c8
Anthony <antosha.1998.ru@mail.ru>
2017-11-11 14:49:23 +0300

    prod

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,7 +34,7 @@ class AppKernel extends Kernel
             new Genemu\Bundle\FormBundle\GenemuFormBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('prod'), true)) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
