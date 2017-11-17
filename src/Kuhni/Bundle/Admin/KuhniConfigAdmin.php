f7795e6e374917339998a671b0e647b95d23652e
Anthony <antosha.1998.ru@mail.ru>
2017-11-17 15:09:55 +0300

    Добавление салонов в отдельную таблицу. Русская версия tinymce. Стилизация кнопок для слайдера tags. Вывод статей в форму - кухни материалы, цвет, конфигурация, стили.

--- src/Kuhni/Bundle/Admin/KuhniConfigAdmin.php
+++ src/Kuhni/Bundle/Admin/KuhniConfigAdmin.php
@@ -27,6 +27,25 @@ class KuhniConfigAdmin extends AbstractAdmin
             ->add('keywords', TextType::class, array(
                 'label' => 'Ключевые слова'
             ))
+            ->add('article', 'genemu_tinymce', array(
+                'label' => 'Статья',
+                'configs' => array(
+                    'add_unload_trigger' => 'false',
+                    'remove_linebreaks' => 'true',
+                    'inline_styles' => 'true',
+                    'convert_fonts_to_spans' => 'true',
+                    'elements' => "content_editor",
+                    'plugins' => "autolink,lists,spellchecker,pagebreak,table,preview,save,insertdatetime,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking",
+                    'theme_advanced_buttons1' => "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+                    'theme_advanced_buttons2' => "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,uploads_image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+                    'theme_advanced_buttons3' => "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+                    'theme_advanced_buttons4' => "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,blockquote,pagebreak,|,insertfile,insertimage",
+                    'theme_advanced_toolbar_location' => "top",
+                    'theme_advanced_toolbar_align' => "left",
+                    'theme_advanced_statusbar_location' => "bottom",
+                    'theme_advanced_resizing' => true,
+                )
+            ))
             ->add('slug', TextType::class)
             ->add('imageFile', VichImageType::class, array(
                 'required'      => false,
@@ -70,6 +89,9 @@ class KuhniConfigAdmin extends AbstractAdmin
             ->add('imageName', null, array(
                 'label' => 'Картинка',
             ))
+            ->add('article', null, array(
+                'label' => 'Статья'
+            ))
             ->add('slug')
             ->add('_action', 'actions', array(
                 'actions' => array(
@@ -96,6 +118,9 @@ class KuhniConfigAdmin extends AbstractAdmin
             ))
             ->add('imageName', null, array(
                 'label' => 'Картинка',
+            ))
+            ->add('article', null, array(
+                'label' => 'Статья'
             ))
             ->add('slug');
     }
--- src/Kuhni/Bundle/Admin/KuhniMaterialAdmin.php
+++ src/Kuhni/Bundle/Admin/KuhniMaterialAdmin.php
@@ -63,6 +63,25 @@ class KuhniMaterialAdmin extends AbstractAdmin
             ->add('keywords', TextType::class, array(
                 'label' => 'Ключевые слова'
             ))
+            ->add('article', 'genemu_tinymce', array(
+                'label' => 'Статья',
+                'configs' => array(
+                    'add_unload_trigger' => 'false',
+                    'remove_linebreaks' => 'true',
+                    'inline_styles' => 'true',
+                    'convert_fonts_to_spans' => 'true',
+                    'elements' => "content_editor",
+                    'plugins' => "autolink,lists,spellchecker,pagebreak,table,preview,save,insertdatetime,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking",
+                    'theme_advanced_buttons1' => "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+                    'theme_advanced_buttons2' => "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,uploads_image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+                    'theme_advanced_buttons3' => "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+                    'theme_advanced_buttons4' => "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,blockquote,pagebreak,|,insertfile,insertimage",
+                    'theme_advanced_toolbar_location' => "top",
+                    'theme_advanced_toolbar_align' => "left",
+                    'theme_advanced_statusbar_location' => "bottom",
+                    'theme_advanced_resizing' => true,
+                )
+            ))
             ->add('slug', TextType::class)
             ->add('imageFile', VichImageType::class, array(
                 'required'      => false,
@@ -124,25 +143,6 @@ class KuhniMaterialAdmin extends AbstractAdmin
             ))
             ->add('keywords', null, array(
                 'label'    => 'Ключевые слова'
-            ))
-            ->add('article', 'genemu_tinymce', array(
-                'label' => 'Статья',
-                'configs' => array(
-                    'add_unload_trigger' => 'false',
-                    'remove_linebreaks' => 'true',
-                    'inline_styles' => 'true',
-                    'convert_fonts_to_spans' => 'true',
-                    'elements' => "content_editor",
-                    'plugins' => "autolink,lists,spellchecker,pagebreak,table,preview,save,insertdatetime,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking",
-                    'theme_advanced_buttons1' => "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-                    'theme_advanced_buttons2' => "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,uploads_image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-                    'theme_advanced_buttons3' => "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-                    'theme_advanced_buttons4' => "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,blockquote,pagebreak,|,insertfile,insertimage",
-                    'theme_advanced_toolbar_location' => "top",
-                    'theme_advanced_toolbar_align' => "left",
-                    'theme_advanced_statusbar_location' => "bottom",
-                    'theme_advanced_resizing' => true,
-                )
             ));
     }
     // Fields to be shown on lists
