413dfc0af82ad4e4f72385057de17ce607ff20a3
Anton <antosha.1998.ru@mail.ru>
2017-11-19 22:06:04 +0300

    swift mailer

--- src/Kuhni/Bundle/Resources/public/js/ajax.js
+++ src/Kuhni/Bundle/Resources/public/js/ajax.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
     //обыное сообщение   privacy-politycs
     $('body').on('submit', '.ajaxForm', function (e) {
         e.preventDefault();
-        if ($(this).find('input[name="privacy-politycs"]').prop('checked')) {
+        if ($(this).attr('id') == 'requestCallForm'){
             $('form button').attr("disabled", "disabled");
             $.ajax({
                 type: $(this).attr('method'),
@@ -93,11 +93,49 @@ $(document).ready(function(){
                 $('form button').removeAttr("disabled", "disabled");
             });
         }else{
-            $(this).find('button[type="submit"]').after(
-                '<div class="alert alert-info" role="alert">' +
-                '<strong>Необходимо подтвердить политику конфиденциальности!</strong>' +
-                '</div>'
-            );
+            if ($(this).find('input[name="privacy-politycs"]').prop('checked')) {
+                $('form button').attr("disabled", "disabled");
+                $.ajax({
+                    type: $(this).attr('method'),
+                    url: $(this).attr('action'),
+                    data: $(this).serialize()
+                }).done(function (data, status) {
+                    $('form').trigger('reset');
+                    if ((data['success'] = 'success') && ((status = 'success'))) {
+                        $('form button[type="submit"]').after(
+                            '<div class="alert alert-success" role="alert">' +
+                            '<strong>Спасибо!</strong> Ваше сообщение отправлено.' +
+                            '</div>'
+                        );
+                    } else {
+                        $('form button[type="submit"]').after(
+                            '<div class="alert alert-error" role="alert">' +
+                            '<strong>Что-то пошло не так!</strong>' +
+                            '</div>'
+                        );
+                    }
+                    $('form button').removeAttr("disabled", "disabled");
+                    setTimeout(function () {
+                        $('.alert').fadeOut('fast')
+                    }, 3000);
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (typeof jqXHR.responseJSON !== 'undefined') {
+                        if (jqXHR.responseJSON.hasOwnProperty('form')) {
+                            $('#form_body').html(jqXHR.responseJSON.form);
+                        }
+                        $('.form_error').html(jqXHR.responseJSON.message);
+                    } else {
+                        alert(errorThrown);
+                    }
+                    $('form button').removeAttr("disabled", "disabled");
+                });
+            }else{
+                $(this).find('button[type="submit"]').after(
+                    '<div class="alert alert-info" role="alert">' +
+                    '<strong>Необходимо подтвердить политику конфиденциальности!</strong>' +
+                    '</div>'
+                );
+            }
         }
     });
 });
