9e2c882d0a90d7a9604f658d322497b94342ff45
Anthony <antosha.1998.ru@mail.ru>
2017-11-20 13:21:17 +0300

    Вывод отзывов.

--- src/Kuhni/Bundle/Controller/ReviewsController.php
+++ src/Kuhni/Bundle/Controller/ReviewsController.php
@@ -418,12 +418,10 @@ class ReviewsController extends Controller
     private function getMapLocate()
     {
         $em = $this->getDoctrine()->getManager()
-            ->getRepository('ApplicationSonataUserBundle:User');
+            ->getRepository('KuhniBundle:Salon');
         $qb = $em->createQueryBuilder('u');
         $locate =
-            $qb->select()
-                ->where('u.salon = 1')
-                ->orderBy('u.id', 'ASC');
+            $qb->where('u.vivodKarta = 1')->orderBy('u.id', 'ASC');
         return $locate->getQuery()->getResult();
     }
 }
